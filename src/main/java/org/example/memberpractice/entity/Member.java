be766129c15fd6aace48c0f162d00186e1730cea
heeheepark <pjh9641@gmail.com>
2026-07-09 13:25:08 +0900

    feat: Member Entity 생성

---
+++ src/main/java/org/example/memberpractice/entity/Member.java
@@ -0,0 +1,21 @@
+package org.example.memberpractice.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "members")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+}
